e061dfa0efc6b4c47b72463ff4e4534e1ec29723
Michael Bestas <mikeioannina@gmail.com>
2013-08-07 19:04:23 +0300

    Add new cherry-picks

--- updates.sh
+++ updates.sh
@@ -8,3 +8,13 @@ fi
 cd ${android}/hardware/qcom/audio-caf
 # audio/msm7x30: Updates for MR2
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_qcom_audio-caf refs/changes/87/46087/2 && git cherry-pick FETCH_HEAD
+
+cd ${android}/frameworks/av
+# libstagefright: Add legacy LPAPlayer
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/51/46451/3 && git cherry-pick FETCH_HEAD
+# libstagefright: support for disabling buffer metadata
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/95/46495/1 && git cherry-pick FETCH_HEAD
+
+cd ${android}/frameworks/native
+# surfaceflinger: enable EGL_NEEDS_FNW for qcom legacy
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_native refs/changes/59/47259/1 && git cherry-pick FETCH_HEAD
