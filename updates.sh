78299b4c448ef9b774e17f05d58aa5cf7c9e11af
Michael Bestas <mikeioannina@gmail.com>
2013-08-09 11:35:21 +0300

    Remove merged patch

--- updates.sh
+++ updates.sh
@@ -13,8 +13,6 @@ cd ${android}/hardware/qcom/audio-caf
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_qcom_audio-caf refs/changes/87/46087/2 && git cherry-pick FETCH_HEAD
 
 cd ${android}/frameworks/av
-# libstagefright: Add legacy LPAPlayer
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/51/46451/3 && git cherry-pick FETCH_HEAD
 # libstagefright: support for disabling buffer metadata
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/95/46495/1 && git cherry-pick FETCH_HEAD
 
