7fd9f5ea7875b397515cfa4a906974126b0ab817
Michael Bestas <mikeioannina@gmail.com>
2013-08-14 19:23:38 +0300

    Add optimization patches
    Also remove merged patches

--- updates.sh
+++ updates.sh
@@ -9,11 +9,18 @@ cd ${android}/system/core
 # init: Fix serial number on semc bootloaders
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_core refs/changes/74/38174/1 && git cherry-pick FETCH_HEAD
 
-
 cd ${android}/bootable/recovery
 # "not enough rainbows, 1 star uninstall"
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery refs/changes/77/36777/3 && git cherry-pick FETCH_HEAD
-# Update wipe data option confirmation
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery refs/changes/37/45437/6 && git cherry-pick FETCH_HEAD
-# Add sdparted option to partition in ext4 fstype
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery refs/changes/57/45557/2 && git cherry-pick FETCH_HEAD
+
+cd ${android}/frameworks/native
+# Configure dalvik heap parameters for low memory devices
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_native refs/changes/85/47285/1 && git cherry-pick FETCH_HEAD
+
+cd ${android}/dalvik
+# dalvik/vm: Dalvik startup with a low memory footprint
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_dalvik refs/changes/57/47757/1 && git cherry-pick FETCH_HEAD
+
+cd ${android}/frameworks/base
+# Reduce total number of background apps for low memory devices
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/88/47288/1 && git cherry-pick FETCH_HEAD
