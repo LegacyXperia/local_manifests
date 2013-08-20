26e464f08236b250bdc18e61bc25a32e8501117f
Michael Bestas <mikeioannina@gmail.com>
2013-08-20 23:50:23 +0300

    Bring the rainbows back!

--- updates.sh
+++ updates.sh
@@ -8,6 +8,10 @@ cd ${android}/system/core
 # init: Fix serial number on semc bootloaders
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_core refs/changes/74/38174/1 && git cherry-pick FETCH_HEAD
 
+cd ${android}/bootable/recovery
+# "not enough rainbows, 1 star uninstall"
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery refs/changes/77/36777/3 && git cherry-pick FETCH_HEAD
+
 cd ${android}/hardware/qcom/audio-caf
 # msm7x30: use policy manager from ALSA
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_qcom_audio-caf refs/changes/84/47484/2 && git cherry-pick FETCH_HEAD
