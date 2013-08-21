f718d438e29f7215d902407d74872da4c4d9a0f0
Michael Bestas <mikeioannina@gmail.com>
2013-08-21 07:15:07 +0300

    Add camera patch/fix

--- updates.sh
+++ updates.sh
@@ -13,6 +13,10 @@ cd ${android}/bootable/recovery
 # "not enough rainbows, 1 star uninstall"
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery refs/changes/77/36777/3 && git cherry-pick FETCH_HEAD
 
+cd ${android}/frameworks/av
+# camera: Fix preview on SEMC msm7x30 devices
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/75/48675/1 && git cherry-pick FETCH_HEAD
+
 cd ${android}/frameworks/native
 # Configure dalvik heap parameters for low memory devices
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_native refs/changes/85/47285/1 && git cherry-pick FETCH_HEAD
