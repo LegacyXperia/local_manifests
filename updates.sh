8600e9e08762772aadf7f110a653c1212e334c8d
Michael Bestas <mikeioannina@gmail.com>
2013-08-21 05:18:25 +0300

    Add camera patch/fix

--- updates.sh
+++ updates.sh
@@ -21,6 +21,8 @@ git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_qcom_audio-
 cd ${android}/frameworks/av
 # libstagefright: support for disabling buffer metadata
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/95/46495/1 && git cherry-pick FETCH_HEAD
+# camera: Fix preview on SEMC msm7x30 devices
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/73/48673/1 && git cherry-pick FETCH_HEAD
 
 cd ${android}/frameworks/native
 # Don't use fence sync for qcom legacy
