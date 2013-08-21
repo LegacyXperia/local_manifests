4e3cf599eec48ae351ff1503ed0ef68a49dafeb6
Michael Bestas <mikeioannina@gmail.com>
2013-08-21 03:43:50 +0300

    Update patches
    Add back some patches & new audio patch

--- updates.sh
+++ updates.sh
@@ -15,10 +15,18 @@ git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery re
 cd ${android}/hardware/qcom/audio-caf
 # msm7x30: use policy manager from ALSA
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_qcom_audio-caf refs/changes/84/47484/2 && git cherry-pick FETCH_HEAD
+# msm7x30: disable logcat spam
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_qcom_audio-caf refs/changes/02/48202/1 && git cherry-pick FETCH_HEAD
 
 cd ${android}/frameworks/av
 # libstagefright: support for disabling buffer metadata
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/95/46495/1 && git cherry-pick FETCH_HEAD
+
+cd ${android}/frameworks/native
+# Don't use fence sync for qcom legacy
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_native refs/changes/80/47180/1 && git cherry-pick FETCH_HEAD
+# surfaceflinger: enable EGL_NEEDS_FNW for qcom legacy
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_native refs/changes/59/47259/1 && git cherry-pick FETCH_HEAD
 
 cd ${android}/dalvik
 # dalvik/vm: Dalvik startup with a low memory footprint
