010c42be72780e4b27dd3712fe82f452af5663ee
Michael Bestas <mikeioannina@gmail.com>
2013-09-05 00:49:22 +0300

    Add xperia play touchpads patch
    Also remove merged camera patch

--- updates.sh
+++ updates.sh
@@ -4,7 +4,6 @@ if [ "${android}" = "" ]; then
 	android=~/android/system
 fi
 
-
 cd ${android}/system/core
 # init: Fix serial number on semc bootloaders
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_core refs/changes/74/38174/1 && git cherry-pick FETCH_HEAD
@@ -13,9 +12,9 @@ cd ${android}/bootable/recovery
 # "not enough rainbows, 1 star uninstall"
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery refs/changes/77/36777/3 && git cherry-pick FETCH_HEAD
 
-cd ${android}/frameworks/av
-# camera: Fix preview on SEMC msm7x30 devices
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/75/48675/1 && git cherry-pick FETCH_HEAD
+cd ${android}/frameworks/base
+# Add support for Xperia Play touchpads
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/03/45903/4 && git cherry-pick FETCH_HEAD
 
 cd ${android}/frameworks/native
 # Configure dalvik heap parameters for low memory devices
