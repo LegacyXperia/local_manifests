8c2a6cad470412e1799a773f5073fb563445b506
Michael Bestas <mikeioannina@gmail.com>
2013-09-12 18:00:49 +0300

    Remove merged patches

--- updates.sh
+++ updates.sh
@@ -19,15 +19,3 @@ git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery re
 cd ${android}/frameworks/base
 # Add support for Xperia Play touchpads
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/03/45903/4 && git cherry-pick FETCH_HEAD
-
-cd ${android}/frameworks/native
-# Configure dalvik heap parameters for low memory devices
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_native refs/changes/85/47285/1 && git cherry-pick FETCH_HEAD
-
-cd ${android}/dalvik
-# dalvik/vm: Dalvik startup with a low memory footprint
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_dalvik refs/changes/57/47757/1 && git cherry-pick FETCH_HEAD
-
-cd ${android}/packages/apps/Torch
-# Torch: only write to sysfs if not already on
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_packages_apps_Torch refs/changes/74/49874/1 && git cherry-pick FETCH_HEAD
