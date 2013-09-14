9e3bf5294c48d45786b11bdeaa2c6e73bf1f0b8c
Michael Bestas <mikeioannina@gmail.com>
2013-09-14 03:57:00 +0300

    Remove merged patches

--- updates.sh
+++ updates.sh
@@ -21,11 +21,5 @@ cd ${android}/frameworks/base
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/10/49310/1 && git cherry-pick FETCH_HEAD
 
 cd ${android}/frameworks/av
-# libstagefright: support for disabling buffer metadata
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/95/46495/1 && git cherry-pick FETCH_HEAD
 # camera: Fix preview on SEMC msm7x30 devices
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/73/48673/1 && git cherry-pick FETCH_HEAD
-
-cd ${android}/packages/apps/Torch
-# Torch: only write to sysfs if not already on
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_packages_apps_Torch refs/changes/28/49628/7 && git cherry-pick FETCH_HEAD
