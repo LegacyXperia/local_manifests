5d4c1c09c23bbb39346b76409ea597d99f6bf3fb
Michael Bestas <mikeioannina@gmail.com>
2013-09-22 03:16:31 +0300

    Add new wlan patches

--- updates.sh
+++ updates.sh
@@ -23,3 +23,9 @@ git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/01/50201/2 && git cherry-pick FETCH_HEAD
 # Fix status bar brightness control when on secure lock screen
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/95/50695/2 && git cherry-pick FETCH_HEAD
+
+cd ${android}/hardware/ti/wlan
+# WLAN - compat_wl12xx: more file cleanups
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/69/50769/1 && git cherry-pick FETCH_HEAD
+# WLAN - wl12xx: Update to R5.SP5.01 from R5.SP3.06 package
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/72/50772/1 && git cherry-pick FETCH_HEAD
