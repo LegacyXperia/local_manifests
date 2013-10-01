b32ac3d1e4eeaecdfc5187c4dcba7d52374025a2
Michael Bestas <mikeioannina@gmail.com>
2013-10-01 16:05:55 +0300

    Add wlan patches
    also remove libloc_api patch

--- updates.sh
+++ updates.sh
@@ -34,9 +34,29 @@ cd ${android}/frameworks/av
 # camera: Fix preview on SEMC msm7x30 devices
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/73/48673/1 && git cherry-pick FETCH_HEAD
 
-cd ${android}/hardware/qcom/gps
-# libloc_api: Fix some compiler warnings
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_qcom_gps refs/changes/53/49853/1 && git cherry-pick FETCH_HEAD
+cd ${android}/hardware/ti/wlan
+# compat-wireless: turn on TESTMODE and MAC DEBUGFS, turn off BT
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/30/51130/1 && git cherry-pick FETCH_HEAD
+# hostapd.conf: setting wlan1 as default interface
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/31/51131/1 && git cherry-pick FETCH_HEAD
+# hostapd.conf: added greenfield support
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/32/51132/1 && git cherry-pick FETCH_HEAD
+# hostapd.conf: add RX-STBC1 support
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/33/51133/1 && git cherry-pick FETCH_HEAD
+# wl12xx: Import firmware & ini files from ol_R5.SP5.01
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/24/51224/1 && git cherry-pick FETCH_HEAD
+
+cd ${android}/system/netd
+# SoftapControllerTI: enable advanced SoftAP solution
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_netd refs/changes/34/51134/1 && git cherry-pick FETCH_HEAD
+# SoftapControllerTI: set correct permissions on conf file
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_netd refs/changes/35/51135/1 && git cherry-pick FETCH_HEAD
+# SoftapControllerTI: override ctrl_interface with correct interface
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_netd refs/changes/36/51136/1 && git cherry-pick FETCH_HEAD
+
+cd ${android}/external/wpa_supplicant_8_ti
+# Squashed update to ol_R5.SP5.01
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_external_wpa_supplicant_8_ti refs/changes/29/51129/1 && git cherry-pick FETCH_HEAD
 
 cd ${android}/build
 # build: remove VideoEditor & live wallpapers from full_base
