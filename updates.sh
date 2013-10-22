7884e1eb589fc9e875d86fe3590ecec7b6c06087
Michael Bestas <mikeioannina@gmail.com>
2013-10-22 07:32:40 +0300

    Remove patches

--- updates.sh
+++ updates.sh
@@ -8,12 +8,6 @@ if [ "${android}" = "" ]; then
 	android=~/android/system
 fi
 
-cd ${android}/bootable/recovery
-# fix wired bugs due to new fs_mgr, vold and virtual device paths
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery refs/changes/12/51312/9 && git cherry-pick FETCH_HEAD
-# - friendly write_recovery_version()
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_bootable_recovery refs/changes/06/51706/1 && git cherry-pick FETCH_HEAD
-
 cd ${android}/frameworks/base
 # Add support for Xperia Play touchpads
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/10/49310/1 && git cherry-pick FETCH_HEAD
@@ -27,22 +21,8 @@ git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/c
 cd ${android}/hardware/ti/wlan
 # compat-wireless: turn on TESTMODE and MAC DEBUGFS, turn off BT
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/30/51130/1 && git cherry-pick FETCH_HEAD
-# hostapd.conf: setting wlan1 as default interface
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/31/51131/1 && git cherry-pick FETCH_HEAD
-# hostapd.conf: added greenfield support
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/32/51132/1 && git cherry-pick FETCH_HEAD
-# hostapd.conf: add RX-STBC1 support
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/33/51133/1 && git cherry-pick FETCH_HEAD
 # wl12xx: Import firmware & ini files from ol_R5.SP5.01
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/24/51224/1 && git cherry-pick FETCH_HEAD
-
-cd ${android}/system/netd
-# SoftapControllerTI: enable advanced SoftAP solution
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_netd refs/changes/34/51134/1 && git cherry-pick FETCH_HEAD
-# SoftapControllerTI: set correct permissions on conf file
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_netd refs/changes/35/51135/1 && git cherry-pick FETCH_HEAD
-# SoftapControllerTI: override ctrl_interface with correct interface
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_netd refs/changes/36/51136/1 && git cherry-pick FETCH_HEAD
 
 cd ${android}/external/wpa_supplicant_8_ti
 # Squashed update to ol_R5.SP5.01
