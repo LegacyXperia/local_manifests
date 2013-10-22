89487f13fadc501398ca2969edaf4d1adff2373b
Michael Bestas <mikeioannina@gmail.com>
2013-10-22 07:33:09 +0300

    Remove patches

--- updates.sh
+++ updates.sh
@@ -21,22 +21,8 @@ git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs
 cd ${android}/hardware/ti/wlan
 # compat-wireless: turn on TESTMODE and MAC DEBUGFS, turn off BT
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/99/51099/1 && git cherry-pick FETCH_HEAD
-# hostapd.conf: setting wlan1 as default interface
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/00/51100/1 && git cherry-pick FETCH_HEAD
-# hostapd.conf: added greenfield support
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/01/51101/1 && git cherry-pick FETCH_HEAD
-# hostapd.conf: add RX-STBC1 support
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/02/51102/1 && git cherry-pick FETCH_HEAD
 # wl12xx: Import firmware & ini files from ol_R5.SP5.01
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/18/51218/2 && git cherry-pick FETCH_HEAD
-
-cd ${android}/system/netd
-# SoftapControllerTI: enable advanced SoftAP solution
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_netd refs/changes/55/51155/2 && git cherry-pick FETCH_HEAD
-# SoftapControllerTI: set correct permissions on conf file
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_netd refs/changes/21/51221/1 && git cherry-pick FETCH_HEAD
-# SoftapControllerTI: override ctrl_interface with correct interface
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_netd refs/changes/22/51222/1 && git cherry-pick FETCH_HEAD
 
 cd ${android}/external/wpa_supplicant_8_ti
 # Squashed update to ol_R5.SP5.01
