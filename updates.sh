0238dcf4a3faea5b46ef772ae1d862d1ca18cd3b
Michael Bestas <mikeioannina@gmail.com>
2013-11-04 16:22:43 +0200

    Update wlan patches

--- updates.sh
+++ updates.sh
@@ -30,9 +30,11 @@ cd ${android}/hardware/ti/wlan
 # compat-wireless: turn on TESTMODE and MAC DEBUGFS, turn off BT
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/30/51130/1 && git cherry-pick FETCH_HEAD
 # wl12xx: Import firmware & ini files from ol_R5.SP5.01
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/24/51224/5 && git cherry-pick FETCH_HEAD
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/24/51224/6 && git cherry-pick FETCH_HEAD
 # wpa_supplicant_lib: add check for TI specific wpa_supplicant
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/07/52707/1 && git cherry-pick FETCH_HEAD
+# ti/wlan: Remove obsolete files
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_ti_wlan refs/changes/88/52888/1 && git cherry-pick FETCH_HEAD
 
 cd ${android}/hardware/libhardware_legacy
 # wifi: remove p2p interface on supplicant stop
