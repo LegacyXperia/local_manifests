29a127dcec6558e313b335974512c395fa1102b5
Michael Bestas <mikeioannina@gmail.com>
2013-11-27 11:08:21 +0200

    Update patches

--- updates.sh
+++ updates.sh
@@ -7,30 +7,17 @@ fi
 if [ "${android}" = "" ]; then
 	android=~/android/system
 fi
-
-# Add support for Xperia Play touchpads
-cherries+=(49310)
-# Lower the RAM requirement for hardware acceleration
-cherries+=(51488)
 # camera: Fix preview on SEMC msm7x30 devices
 cherries+=(48673)
-# wpa_supplicant_lib: add check for TI specific wpa_supplicant
-cherries+=(52707)
-# wifi: remove p2p interface on supplicant stop
-cherries+=(52530)
+
 # Clean up makefiles
 cherries+=(52708)
 # wpan: fix build issues
 cherries+=(52709)
-# ti-wpan-nfc-products.mk: Adding the product packages required for NFC
-cherries+=(52710)
-# Squashed update of kfmapp from d-jb-mr1-release
-cherries+=(52711)
-# Squashed update of FM app from d-jb-mr1-release
-cherries+=(52712)
-# ti-wpan-fm-products.mk: Adding the product packages required for FM
-cherries+=(52713)
-# msm7x30: Add SEMC devices
+# wpan: Enable flag check for FM app
+cherries+=(53699)
+
+# msm7x30: Add support for camcorder mic
 cherries+=(53435)
 
 ${android}/build/tools/repopick.py -b ${cherries[@]}
