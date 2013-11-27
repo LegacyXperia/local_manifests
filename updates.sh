88b805d9c4e92164057daf32d31005840e9bbfd0
Michael Bestas <mikeioannina@gmail.com>
2013-11-27 11:09:18 +0200

    Update patches

--- updates.sh
+++ updates.sh
@@ -8,9 +8,6 @@ if [ "${android}" = "" ]; then
 	android=~/android/system
 fi
 
-# Add support for Xperia Play touchpads
-cherries+=(49310)
-
 # camera: Fix preview on SEMC msm7x30 devices
 cherries+=(54582)
 
@@ -24,19 +21,9 @@ cherries+=(53162)
 # Use AOSP version of wpa_supplicant_lib
 cherries+=(54289)
 
-# Clean up makefiles
-cherries+=(54583)
-# wpan: fix build issues
-cherries+=(54584)
-# wpan: Enable flag check for FM app
-cherries+=(54585)
-
 # vold: save fat partition volume id to system prop
 cherries+=(54610)
 # MediaProvider: read fat volume id from prop
 cherries+=(54609)
-
-# linker: Avoid logcat spam with some blobs
-cherries+=(53390)
 
 ${android}/build/tools/repopick.py -b ${cherries[@]}
