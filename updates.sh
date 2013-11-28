dd694a9a0ee0b07205db93d9acc3ccff0e99694e
Michael Bestas <mikeioannina@gmail.com>
2013-11-28 03:02:25 +0200

    Remove patch

--- updates.sh
+++ updates.sh
@@ -20,7 +20,4 @@ cherries+=(53699)
 # msm7x30: Add support for camcorder mic
 cherries+=(53435)
 
-# Revert "Add Andy's ear wiggle to recovery"
-cherries+=(54693)
-
 ${android}/build/tools/repopick.py -b ${cherries[@]}
