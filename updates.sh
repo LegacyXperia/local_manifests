a5bf4538feb4962c1f2d10c4c9557467260248b4
Michael Bestas <mikeioannina@gmail.com>
2013-12-16 02:50:07 +0200

    Update patches

--- updates.sh
+++ updates.sh
@@ -18,30 +18,18 @@ cherries+=(54257)
 # libgui: Bring back support for mHeap-based screenshots
 cherries+=(53162)
 
-# Use AOSP version of wpa_supplicant_lib
-cherries+=(54289)
-
 # Add option for switching between UMS and MTP/PTP mode. (2/2)
 cherries+=(54903)
 
-# Settings: Add missing up navigation in menus
-cherries+=(54957)
-
 # Phone: implement noise suppression for phone calls (2/2)
 cherries+=(55075)
-
-# Fix a bootloop problem when /data/system/usagestats/usage-history.xml is empty.
-cherries+=(55385)
-
-# Gallery : Filter picasa specific options
-cherries+=(55471)
-
-# Parse UUID string with Long.parseLong
-cherries+=(55512)
 
 # CidKat: CyanogenMod version preference
 cherries+=(55037)
 # CidKat: Changes to toggle between CM and KitKat
 cherries+=(55041)
+
+# SoftapControllerTI: update parameters for setSoftap
+cherries+=(55805)
 
 ${android}/build/tools/repopick.py -b ${cherries[@]}
