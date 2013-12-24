34c83877ba29315eb1ad9618d7a00015a02b51d0
Michael Bestas <mikeioannina@gmail.com>
2013-12-24 23:47:42 +0200

    Massive update

--- updates.sh
+++ updates.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-if [ -f ~/bin/paths-10.2.sh ]; then
-	source ~/bin/paths-10.2.sh
+if [ -f ~/bin/paths-11.0.sh ]; then
+	source ~/bin/paths-11.0.sh
 fi
 
 if [ "${android}" = "" ]; then
 	android=~/android/system
 fi
 
+cd ${android}/build
+# RepoPick: Actually skip merged commit instead of exiting
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_build refs/changes/15/56215/1 && git cherry-pick FETCH_HEAD
+# Fix Repopick Errors
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_build refs/changes/89/56189/1 && git cherry-pick FETCH_HEAD
+
+cd ${android}
 # camera: Fix preview on SEMC msm7x30 devices
 cherries+=(54582)
 
@@ -18,16 +25,33 @@ cherries+=(54257)
 # libgui: Bring back support for mHeap-based screenshots
 cherries+=(53162)
 
-# Add option for switching between UMS and MTP/PTP mode. (2/2)
-cherries+=(54903)
-
-# Phone: implement noise suppression for phone calls (2/2)
-cherries+=(55075)
-
-# CidKat: CyanogenMod version preference
+# CM PlatLogo: CyanogenMod version preference
 cherries+=(55037)
-# CidKat: Changes to toggle between CM and KitKat
+# Settings: Add CM PlatLogo & CMCaseView
 cherries+=(55041)
+
+# Forward port Status Bar interface (1/2)
+cherries+=(55209)
+# Forward port Status Bar settings (2/2)
+cherries+=(55211)
+
+# Adds ability to answer call with hardware HOME button
+cherries+=(56094)
+
+# [WIP] Forward port sound settings (1/2)
+cherries+=(56095)
+
+# Navbar: Fix MDPI & HDPI landscape layout
+cherries+=(56098)
+
+# Squashed fixes for Statusbar Clock and Date actions
+cherries+=(56100)
+
+# Apollo: Add option for sorting by filename into 'Sort by' menu
+cherries+=(54722)
+
+# libc: Unbreak build for scorpion
+cherries+=(56062)
 
 # SoftapControllerTI: update parameters for setSoftap
 cherries+=(55805)
