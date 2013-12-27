30bd79f63ddb9c3fd498b75e17f46057f18e47ff
Michael Bestas <mikeioannina@gmail.com>
2013-12-27 17:34:58 +0200

    Testing fix for timelapse

--- updates.sh
+++ updates.sh
@@ -7,7 +7,11 @@ fi
 if [ "${android}" = "" ]; then
 	android=~/android/system
 fi
+
 # camera: Fix preview on SEMC msm7x30 devices
 cherries+=(48673)
+
+# libstagefright: extend support for disabling buffer metadata
+cherries+=(56416)
 
 ${android}/build/tools/repopick.py -b ${cherries[@]}
