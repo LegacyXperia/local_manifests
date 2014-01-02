2025ee3d0d1b62d2d3134a6b39380c91d7424e6c
Michael Bestas <mikeioannina@gmail.com>
2014-01-02 02:56:20 +0200

    Update patches

--- updates.sh
+++ updates.sh
@@ -11,8 +11,6 @@ fi
 cd ${android}/build
 # RepoPick: Actually skip merged commit instead of exiting
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_build refs/changes/15/56215/1 && git cherry-pick FETCH_HEAD
-# Fix Repopick Errors
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_build refs/changes/89/56189/1 && git cherry-pick FETCH_HEAD
 
 cd ${android}
 # camera: Fix preview on SEMC msm7x30 devices
@@ -25,8 +23,6 @@ cherries+=(56361)
 cherries+=(52994)
 # DisplayDevice: Backwards compatibility with old EGL
 cherries+=(54257)
-# libgui: Bring back support for mHeap-based screenshots
-cherries+=(53162)
 
 # CM PlatLogo: CyanogenMod version preference
 cherries+=(55037)
@@ -41,22 +37,13 @@ cherries+=(55211)
 # Adds ability to answer call with hardware HOME button
 cherries+=(56094)
 
-# Navbar: Fix MDPI & HDPI landscape layout
-cherries+=(56098)
-
-# Squashed fixes for Statusbar Clock and Date actions
-cherries+=(56100)
-
 # Apollo: Add option for sorting by filename into 'Sort by' menu
 cherries+=(54722)
-
-# libc: Unbreak build for scorpion
-cherries+=(56062)
 
 # SoftapControllerTI: update parameters for setSoftap
 cherries+=(55805)
 
-# [WIP] Forward port sound settings (1/2)
+# Forward port sound settings
 cherries+=(56095)
 
 ${android}/build/tools/repopick.py -b ${cherries[@]}
