8c544aa55c7a851856b2e3b2d9c721418aecca6e
Michael Bestas <mikeioannina@gmail.com>
2014-01-03 08:35:55 +0200

    Remove merged patches

--- updates.sh
+++ updates.sh
@@ -16,18 +16,10 @@ cd ${android}
 # camera: Fix preview on SEMC msm7x30 devices
 cherries+=(54582)
 
-# libstagefright: support for disabling buffer metadata
-cherries+=(56361)
-
 # gui: Allow devices to disable fence sync
 cherries+=(52994)
 # DisplayDevice: Backwards compatibility with old EGL
 cherries+=(54257)
-
-# audio-caf: resurrect msm7x30 audio
-cherries+=(56472)
-# libstagefright: Add msm7x30 to the legacy alsa audio check
-cherries+=(56502)
 
 # CM PlatLogo: CyanogenMod version preference
 cherries+=(55037)
@@ -42,14 +34,8 @@ cherries+=(55211)
 # Adds ability to answer call with hardware HOME button
 cherries+=(56094)
 
-# Apollo: Add option for sorting by filename into 'Sort by' menu
-cherries+=(54722)
-
 # SoftapControllerTI: update parameters for setSoftap
 cherries+=(55805)
-
-# Forward port sound settings
-cherries+=(56095)
 
 # LockScreen: allow long-press actions from hardware keys (1/2)
 cherries+=(56346)
