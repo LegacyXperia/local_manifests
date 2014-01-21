2670cf599001d02b32addf9d6a3cd62b1e4ad342
Michael Bestas <mikeioannina@gmail.com>
2014-01-21 05:23:34 +0200

    patches: Remove everything
    CM doesn't merge anything in 10.1 anymore... soon cm-10.2 too
    
    I'll fork and add pick patches I need myself...
    
    Change-Id: Ic5897b1ee671542ecdf1339bf1ce9ffc65184c15

--- updates.sh
+++ updates.sh
@@ -8,15 +8,4 @@ if [ "${android}" = "" ]; then
 	android=~/android/system
 fi
 
-# "not enough rainbows, 1 star uninstall"
-cherries+=(36777)
-# Add support for Xperia Play touchpads
-cherries+=(45903)
-# Lower the RAM requirement for hardware acceleration
-cherries+=(51488)
-# libloc_api: Reduce debug level
-cherries+=(51894)
-# librpc: Reduce debug messages
-cherries+=(51896)
-
-${android}/build/tools/repopick.py -b ${cherries[@]}
+#${android}/build/tools/repopick.py -b ${cherries[@]}
