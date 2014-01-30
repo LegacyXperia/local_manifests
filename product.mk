c328e17fdcf7f699b83c59bab46c0d39a5d92fec
Michael Bestas <mikeioannina@gmail.com>
2014-01-30 08:34:02 +0200

    Use LX ID for analytics
    Change-Id: I7a648bd3adae13f172020b03addc0a75045a4c4c
    (cherry picked from commit 546d3301c05ad9b14f4bb8629aeffa7340835fc6)

--- product.mk
+++ product.mk
@@ -12,5 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
+
 PRODUCT_PACKAGES += \
     LegacyXperiaCenter
