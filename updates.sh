1a059c8f873812ba88d640a07cc60788283f6d93
Michael Bestas <mikeioannina@gmail.com>
2014-05-05 22:02:21 +0300

    Add cherry-picks

--- updates.sh
+++ updates.sh
@@ -8,4 +8,31 @@ if [ "${android}" = "" ]; then
 	android=~/android/system
 fi
 
-#${android}/vendor/extra/repopick.py -b ${cherries[@]}
+# Workspace: Quad interpolator
+cherries+=(63036)
+
+# Revert "wpa_supplicant_8 - Hostapd: Android related changes for sockets"
+cherries+=(63203)
+
+# telephony: set additional audio parameters for incall audio (2/2)
+cherries+=(63399)
+
+# bluedroid: increase uhid report buffer size for wiimote
+cherries+=(63389)
+
+# audio: Add A2DP notification support
+cherries+=(63131)
+
+# framework/av: Add Usb AoA v2.0 support
+cherries+=(63410)
+
+# libstagefright: Convert mono to stereo for LPA clips
+cherries+=(63411)
+
+# libstagefright: Stability issue with LPA play back.
+cherries+=(63412)
+
+# libstagefright: LPA playback fails when non-LPA clip is next clip
+cherries+=(63413)
+
+${android}/build/tools/repopick.py -b ${cherries[@]}
