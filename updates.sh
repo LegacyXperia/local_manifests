9f441f2a9c2e86b69f3085451f409a41693b4f9a
Michael Bestas <mikeioannina@gmail.com>
2014-05-29 19:47:28 +0300

    Update cherries

--- updates.sh
+++ updates.sh
@@ -17,22 +17,13 @@ cherries+=(63203)
 # bluedroid: increase uhid report buffer size for wiimote
 cherries+=(63389)
 
-# audio: Add A2DP notification support
-cherries+=(63131)
-
-# framework/av: Add Usb AoA v2.0 support
-cherries+=(63410)
-
-# libstagefright: Convert mono to stereo for LPA clips
-cherries+=(63411)
-
-# libstagefright: Stability issue with LPA play back.
-cherries+=(63412)
-
-# libstagefright: LPA playback fails when non-LPA clip is next clip
-cherries+=(63413)
-
 # Add support for Xperia Play touchpads
 cherries+=(63422)
+
+# Add UUID (re-)scan support for vold-mounted volume (1/2)
+cherries+=(64436)
+
+# Add UUID (re-)scan support for vold-mounted volume (2/2)
+cherries+=(64437)
 
 ${android}/build/tools/repopick.py -b ${cherries[@]}
