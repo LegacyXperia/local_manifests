4f7e9e717cd1b73069b6a8ef83da24260d55334e
Michael Bestas <mikeioannina@gmail.com>
2014-06-16 18:48:36 +0300

    Update tracked repositories & add cherry-picks

--- updates.sh
+++ updates.sh
@@ -8,4 +8,25 @@ if [ "${android}" = "" ]; then
 	android=~/android/system
 fi
 
-#${android}/vendor/extra/repopick.py -b ${cherries[@]}
+# build: Add QCOM target-specific config
+cherries+=(7706)
+
+# Squashed QCOM DirectTrack commits
+cherries+=(7460)
+
+# libhardware: Modify ifdefs for legacy DirectTrack
+cherries+=(7652)
+
+# libstagefright: Allow using camera recording buffer as input for encoder
+cherries+=(8069)
+
+# libstagefright: Fix video encoder input buffer
+cherries+=(8070)
+
+# Revert "Revert "mm-video: venc: Correct a typo in variable name""
+cherries+=(4343)
+
+# Externalize symbol picker options to resources
+cherries+=(7354)
+
+${android}/build/tools/repopick.py -b ${cherries[@]}
