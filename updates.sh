ac936e99bcedf294d0f4671618d0c9b685e14807
Michael Bestas <mikeioannina@gmail.com>
2014-08-23 15:59:17 +0300

    Remove merged patches

--- updates.sh
+++ updates.sh
@@ -8,21 +8,6 @@ if [ "${android}" = "" ]; then
 	android=~/android/system
 fi
 
-# Revert "wpa_supplicant_8 - Hostapd: Android related changes for sockets"
-cherries+=(63203)
-
-# Squashed revert of SoftapControllerTI changes
-cherries+=(69352)
-
-# netd: Don't reload fw if a single fw is supported
-cherries+=(69353)
-
-# wifi: remove local nl80211 header
-cherries+=(69354)
-
-# New call-in/-back functions for Controller to do vendor-specific shutdown
-cherries+=(69355)
-
 # ti: Add interface_mtu request
 cherries+=(69839)
 
