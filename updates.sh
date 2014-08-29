58b3b0f1d6b994b7c4090228318c5beb3b43e3f1
Przemysław Błaszków <pblaszkow5@gmail.com>
2014-08-29 23:26:52 +0200

    Remove merged patch

--- updates.sh
+++ updates.sh
@@ -17,9 +17,6 @@ cherries+=(8902)
 # wifi: remove local nl80211 header
 cherries+=(8904)
 
-# New call-in/-back functions for Controller to do vendor-specific shutdown
-cherries+=(8910)
-
 # ti: Add interface_mtu request
 cherries+=(8998)
 
