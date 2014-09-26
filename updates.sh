09eb48cdee321d824e3f025a13d717ecfaf199ff
Przemysław Błaszków <pblaszkow5@gmail.com>
2014-09-26 10:30:58 +0200

    Remove merged patches

--- updates.sh
+++ updates.sh
@@ -8,26 +8,11 @@ if [ "${android}" = "" ]; then
 	android=~/android/system
 fi
 
-# Squashed revert of SoftapControllerTI changes
-cherries+=(8901)
-
-# netd: Don't reload fw if a single fw is supported
-cherries+=(8902)
-
-# wifi: remove local nl80211 header
-cherries+=(8904)
-
-# ti: Add interface_mtu request
-cherries+=(8998)
-
 # Android: Android.mk: Define ANDROID_P2P
 cherries+=(9062)
 
 # Android: driver_nl80211: implement NoA/p2p_ps/set_wps_ie stubs
 cherries+=(9309)
-
-# Destroy client handles after they're closed
-cherries+=(9314)
 
 # wl12xx: Ported SEMC changes
 cherries+=(9097)
