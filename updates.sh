59f4c6b08d8f804d7a8daebc4b40e2db8fe17cfe
Michael Bestas <mikeioannina@gmail.com>
2014-11-19 04:15:25 +0200

    Add initial cherry-picks

--- updates.sh
+++ updates.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-if [ -f ~/bin/paths-11.0.sh ]; then
-	source ~/bin/paths-11.0.sh
+if [ -f ~/bin/paths-12.0.sh ]; then
+	source ~/bin/paths-12.0.sh
 fi
 
 if [ "${android}" = "" ]; then
 	android=~/android/system
 fi
 
-#${android}/build/tools/repopick.py -b ${cherries[@]}
+# build: recovery: Support adding device-specific items
+cherries+=(76553)
+
+# build: Handle boot images with custom makefile properly
+cherries+=(76919)
+
+# Revert "get rid of MemoryHeapPmem"
+cherries+=(77524)
+
+# mkbootimg: Support 32K 64K and 128K pagesizes
+cherries+=(78055)
+
+${android}/build/tools/repopick.py -b ${cherries[@]}
