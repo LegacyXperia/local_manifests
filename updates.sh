f9a5491342d48331331069ba46b7890c294dbcce
Michael Bestas <mikeioannina@gmail.com>
2015-07-28 20:08:05 +0300

    Remove merged cherries
    Change-Id: I67befe8f4b9837c9ab4fbbe2bdb52d45f732e98f

--- updates.sh
+++ updates.sh
@@ -39,48 +39,6 @@ cherries+=(CM_103549)
 # libstagefright: Disable LPAPlayer verbose logging
 cherries+=(CM_103550)
 
-# memcg: add memory.pressure_level events
-cherries+=(LX_705)
-
-# introduce for_each_thread() to replace the buggy while_each_thread()
-cherries+=(LX_703)
-
-# mm, oom: make dump_tasks public
-cherries+=(LX_710)
-
-# mm: vmpressure: allow in-kernel clients to subscribe for events
-cherries+=(LX_706)
-
-# mm: Add notifier framework for showing memory
-cherries+=(LX_707)
-
-# android/lowmemorykiller: Account for total_swapcache_pages
-cherries+=(LX_693)
-
-# lowmemorykiller: Don't count swap cache pages twice
-cherries+=(LX_695)
-
-# lowmemorykiller: use for_each_thread instead of buggy while_each_thread
-cherries+=(LX_696)
-
-# lowmemorykiller: enhance debug information
-cherries+=(LX_697)
-
-# lowmemorykiller: Dump out slab state information
-cherries+=(LX_698)
-
-# lowmemorykiller: Run the lowmemory notifier when killing
-cherries+=(LX_699)
-
-# lowmemorykiller: Do proper NULL checks
-cherries+=(LX_700)
-
-# lowmemorykiller: adapt to vmpressure
-cherries+=(LX_701)
-
-# msm7x30-common: Enable adaptive LMK
-cherries+=(LX_702)
-
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
 else
