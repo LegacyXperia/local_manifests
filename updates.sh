6a3d6ae143a63481ee9a7008f2bb263ebe4ea5e8
Michael Bestas <mikeioannina@gmail.com>
2015-10-06 21:03:36 +0300

    Initial cm-13.0 changes
    Change-Id: Ia026401bdcd8dfa3bc10b635004b52ae64018b8e

--- updates.sh
+++ updates.sh
@@ -12,39 +12,6 @@ if [ -f ${android}/updates-local.sh ]; then
     source ${android}/updates-local.sh
 fi
 
-# build: Add option to disable block-based ota
-cherries+=(CM_102837)
-
-# Revert "Revert "Reenable support for non-PIE executables""
-cherries+=(CM_92943)
-
-# arm: Allow disabling PIE for dynamically linked executables
-cherries+=(CM_81758)
-
-# libstagefright: Add legacy LPAPlayer
-cherries+=(CM_103507)
-
-# audio: Add A2DP notification support
-cherries+=(CM_103509)
-
-# framework/av: Add Usb AoA v2.0 support
-cherries+=(CM_103510)
-
-# libstagefright: Convert mono to stereo for LPA clips
-cherries+=(CM_103511)
-
-# libstagefright: Stability issue with LPA play back.
-cherries+=(CM_103512)
-
-# frameworks/av: Fix playback for LPA audio when using gapless api
-cherries+=(CM_103513)
-
-# libstagefright: Disable LPAPlayer verbose logging
-cherries+=(CM_103556)
-
-# libstagefright: Update LPAPlayer callbacks
-cherries+=(CM_109096)
-
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
 else
