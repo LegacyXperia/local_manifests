c227e9b847ec06ae2041850bebf395c139bf7bbd
Michael Bestas <mikeioannina@gmail.com>
2015-12-11 17:09:54 +0200

    Go back to cherry-pick list
    * It's hard to keep up our forks up to date with cm
      with monthly security patches going in
    
    Change-Id: I1f3a3e2f18f26fc4b477c49e956f56a3e6823345

--- updates.sh
+++ updates.sh
@@ -12,6 +12,54 @@ if [ -f ${android}/updates-local.sh ]; then
     source ${android}/updates-local.sh
 fi
 
+# libstagefright: Allow using camera recording buffer as input for encoder
+cherries+=(CM_66213)
+
+# libstagefright: Fix video encoder input buffer
+cherries+=(CM_66214)
+
+# audio: Add A2DP notification support
+cherries+=(CM_63131)
+
+# framework/av: Add Usb AoA v2.0 support
+cherries+=(CM_63410)
+
+# libstagefright: Convert mono to stereo for LPA clips
+cherries+=(CM_63411)
+
+# libstagefright: Stability issue with LPA play back.
+cherries+=(CM_63412)
+
+# frameworks/av: Fix playback for LPA audio when using gapless api
+cherries+=(CM_103480)
+
+# libstagefright: Fix LPA default min duration
+cherries+=(CM_103549)
+
+# libstagefright: Disable LPAPlayer verbose logging
+cherries+=(CM_103550)
+
+# libstagefright: Update LPAPlayer callbacks
+cherries+=(CM_109071)
+
+# input: Set explicit ioprio
+cherries+=(CM_123930)
+
+# surfaceflinger: Set explicit ioprio where required
+cherries+=(CM_123932)
+
+# display: Set thread priority using cutils
+cherries+=(CM_123933)
+
+# hwc: Set ioprio for vsync thread
+cherries+=(CM_123934)
+
+# Set IO scheduler using persist property
+cherries+=(CM_123935)
+
+# cutils: Squashed backport of BFQIO cgroups support
+cherries+=(CM_123936)
+
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
 else
