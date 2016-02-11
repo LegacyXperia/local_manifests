46dbd9cac7d48af93252b67a966a1d28d9d68b8d
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-11 02:06:46 +0200

    Add some cherries
    Change-Id: I80023daa4bbdaf8a35bae2632baa5b5b4056614e

--- updates.sh
+++ updates.sh
@@ -21,6 +21,15 @@ cherries+=(CM_117733)
 # arm: Allow disabling PIE for dynamically linked executables
 cherries+=(CM_123032)
 
+# recovery: Add wipe system partition option
+cherries+=(CM_129827)
+
+# Add wakelock option for flashlight
+cherries+=(CM_131995)
+
+# msm7x30: Add SystemUI torch overlay
+cherries+=(LX_1126)
+
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
 else
