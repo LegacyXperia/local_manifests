fc05bb688f20315995e0f3748ced0d60a1df4c68
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-21 23:27:18 +0200

    Update cherries
    Change-Id: I93d83fee6dc6301d9a5d919371f1bfd051773364

--- updates.sh
+++ updates.sh
@@ -27,6 +27,18 @@ cherries+=(CM_129827)
 # datspinner: low resource fake material progress spinner
 cherries+=(LX_1117)
 
+# mogami-common: Remove BLE support
+cherries+=(LX_1051)
+
+# mogami-common: Remove no longer used BTHC_USERIAL_READ_MEM_SIZE
+cherries+=(LX_1052)
+
+# l2cap: Set transport even if BLE_INCLUDED = FALSE
+cherries+=(CM_133778)
+
+# art: fix dex2oat watchdog timeout
+cherries+=(CM_131709)
+
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
 else
