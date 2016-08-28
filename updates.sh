1fcacd183be8e5b16bc2f1f6dfa9efd4c9ff449c
Michael Bestas <mikeioannina@gmail.com>
2016-08-28 16:57:01 +0300

    cm-14.0 time
    Change-Id: I30ac6e1d113e23ba30ec6f8b2994a156b227436d

--- updates.sh
+++ updates.sh
@@ -12,15 +12,6 @@ if [ -f ${android}/updates-local.sh ]; then
     source ${android}/updates-local.sh
 fi
 
-# Use RECOVERY_PRE_COMMAND before calling __reboot() recovery
-cherries+=(CM_115693)
-
-# Revert "Revert "Reenable support for non-PIE executables""
-cherries+=(CM_117733)
-
-# arm: Allow disabling PIE for dynamically linked executables
-cherries+=(CM_123032)
-
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
 else
