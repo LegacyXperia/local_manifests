49141a3a6b548b265dc949b00b8a1118118a4bc1
SteadyQuad <SteadyQuad@gmail.com>
2016-10-07 11:26:45 +0200

    updates.sh: updated
    Change-Id: Ia312109b0848f008344266943589f66adb83468e

--- updates.sh
+++ updates.sh
@@ -15,26 +15,11 @@ fi
 # sad selinux 2002
 cherries+=(CM_164156)
 
-# linker: Make text relocations denial optional
-cherries+=(CM_164155)
-
 # Revert "Revert "Reenable support for non-PIE executables""
 cherries+=(CM_164317)
 
 # arm: Allow disabling PIE for dynamically linked executables
 cherries+=(CM_164318)
-
-# msm7x30: Allow text relocations
-cherries+=(LX_1348)
-
-# msm7x30: Switch cpu variant to cortex-a8
-cherries+=(LX_1349)
-
-# msm7x30: Reduce recovery resource density
-cherries+=(LX_1350)
-
-# policy_hal: Update for N
-cherries+=(LX_1353)
 
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
