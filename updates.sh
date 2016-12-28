1e58c13552846a7128fcd0e0723ce0afc327bd17
Michael Bestas <mikeioannina@gmail.com>
2016-12-28 23:39:30 +0200

    Update cherries for LineageOS gerrit
    Change-Id: I13461e501e2e5ac23ee7342e9e34ea402aae0d33

--- updates.sh
+++ updates.sh
@@ -12,14 +12,17 @@ if [ -f ${android}/updates-local.sh ]; then
     source ${android}/updates-local.sh
 fi
 
-# sad selinux 2002
-cherries+=(LAOS_164156)
+# selinux: add selinux_status_getenforce to header
+cherries+=(LAOS_60713)
+
+# init: don't skip starting a service with no domain if permissive
+cherries+=(LAOS_142846)
 
 # Revert "Revert "Reenable support for non-PIE executables""
-cherries+=(LAOS_167387)
+cherries+=(LAOS_1175)
 
 # arm: Allow disabling PIE for dynamically linked executables
-cherries+=(LAOS_167388)
+cherries+=(LAOS_2643)
 
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
