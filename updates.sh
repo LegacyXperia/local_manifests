30f8fc49811555355c843c8c3db0a808ee680f30
Rudolf Tammekivi <rtammekivi@gmail.com>
2017-02-04 16:03:22 +0200

    Remove SELinux hacks
    Change-Id: I4882c3a63ec29b8526ea6b76801ca7a9062e3414

--- updates.sh
+++ updates.sh
@@ -12,12 +12,6 @@ if [ -f ${android}/updates-local.sh ]; then
     source ${android}/updates-local.sh
 fi
 
-# selinux: add selinux_status_getenforce to header
-cherries+=(LAOS_60713)
-
-# init: don't skip starting a service with no domain if permissive
-cherries+=(LAOS_142846)
-
 # Add LOCAL_NO_FPIE for target executables
 cherries+=(LAOS_159624)
 
